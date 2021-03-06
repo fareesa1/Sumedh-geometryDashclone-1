cac4c5cb0b00582e04dd3da4e6633d88dd771189
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-03-06 16:36:54 +0530

    added bg

--- sketch.js
+++ sketch.js
@@ -1,26 +1,31 @@
-var player_box, ground, background;
+var player_box, ground, background1;
 var player_boxImage, groundImage, backgroundImage;
 
 function preload(){
-  groundImage = loadImage("ground.png")
+  groundImage = loadImage("ground.png");
+  bgImage = loadImage("bg.png")
 }
 
 function setup(){
-  createCanvas(1200,600)
-  //b g
-
-  // ground sprite
+  createCanvas(1200,700)
+ 
+  background1 = createSprite(600,350)
+  background1.addImage(bgImage);
+  background1.scale = 1
+ 
 
   ground = createSprite(600,500)
   ground.scale = 0.7
   ground.velocityX = -7
-  ground.addImage(groundImage)
+  ground.addImage(groundImage);
+  ground.x = ground.width/2;
  
 }
 
 function draw(){
-  background("black")
-  
-
+  background("white")
+  if(ground.x < 0){
+  ground.x = ground.width/2
+}
   drawSprites()
 }
